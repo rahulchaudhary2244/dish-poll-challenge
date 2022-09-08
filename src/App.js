1c57e8a2be037bf83946393386d73579dfa5d3e8
rahulchaudhary2244 <rahulchaudhary2244@gmail.com>
2022-09-08 08:02:19 +0530

    adding theme and snackbar configuration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import './styles.css';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './utils/theme';
+import { SnackbarProvider } from 'notistack';
+
 function App() {
-    return <div className="App">Hello Dishes</div>;
+    return (
+        <div className="App">
+            <ThemeProvider theme={theme}>
+                <SnackbarProvider
+                    dense
+                    preventDuplicate
+                    maxSnack={3}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'center',
+                    }}
+                    autoHideDuration={3000}
+                >
+                    <div>hello</div>
+                </SnackbarProvider>
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
